2f928ba3428f6fe3767155cba187b05f9303642c
mohit <mohitgadhavi1@gmail.com>
2025-09-26 12:24:31 +0000

    solved 2 probs

---
+++ Valid Triangle Number.py
@@ -0,0 +1,21 @@
+
+from typing import List
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        n= len(nums)
+        count = 0
+        
+        for i in range(n-1, 1, -1):
+            left = 0
+            right = i-1
+            
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    count += right - left
+                    right -= 1
+                else:
+                    left += 1
+        return count
+
